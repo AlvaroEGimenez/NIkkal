9ee65f7e8fff52e69aaab762963e2913bddb9d26
Alvaro <alvarogimenez_285@hotmail.com>
2018-10-07 21:12:25 -0300

    Agregada nuevas animaciones
    slice y zoom in

--- app/src/main/java/com/digitalhouse/a0818moacn01_02/InicioActivity.java
+++ app/src/main/java/com/digitalhouse/a0818moacn01_02/InicioActivity.java
@@ -13,17 +13,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class InicioActivity extends AppCompatActivity {
     private ImageView imageViewLogo;
-    private ImageView imageViewLetra;
-    long duracion=2000;
+    long duracion = 2000;
     public static int MILISEGUNDOS_ESPERA = 4000;
     CoordinatorLayout coordinatorLayout;
-
-
 
 
     @Override
@@ -31,25 +31,26 @@ public class InicioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
-
+        Animation animationSlice;
+        animationSlice = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slice_down);
         imageViewLogo = findViewById(R.id.nikkal);
-        imageViewLetra = findViewById(R.id.nikkalLetra);
+        TextView textViewLetra = findViewById(R.id.nikkalLetra);
 
         final Button button = findViewById(R.id.botonoffline);
         coordinatorLayout = findViewById(R.id.cordinator);
 
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            handleAnimationVertical(imageViewLogo);
-            handleAnimationVerticalLetra(imageViewLetra);
+            AnimacionVertical(imageViewLogo);
+            textViewLetra.setAnimation(animationSlice);
         } else {
-            handleAnimationHorizontal(imageViewLogo);
-            handleAnimationHorizontalLetra(imageViewLetra);
+            animacionHorizontal(imageViewLogo);
+            textViewLetra.setAnimation(animationSlice);
         }
 
 
         final Intent intent = new Intent(InicioActivity.this, MainActivity.class);
 
-        if (isNetDisponible()) {
+        if (verificarConexion()) {
             esperarYCerrar(MILISEGUNDOS_ESPERA);
         } else {
             button.setVisibility(View.VISIBLE);
@@ -64,7 +65,7 @@ public class InicioActivity extends AppCompatActivity {
         }
     }
 
-    private boolean isNetDisponible() {
+    private boolean verificarConexion() {
 
         ConnectivityManager connectivityManager = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -75,50 +76,33 @@ public class InicioActivity extends AppCompatActivity {
     }
 
     public void esperarYCerrar(Integer milisegundos) {
-        final Intent intent = new Intent(InicioActivity.this,MainActivity.class);
+        final Intent intent = new Intent(InicioActivity.this, MainActivity.class);
         Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {public void run() {
-            startActivity(intent);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                startActivity(intent);
 
-        }
+            }
         }, milisegundos);
     }
 
-    public void handleAnimationVertical (View view){
-        ObjectAnimator animatorY = ObjectAnimator.ofFloat(imageViewLogo,"y",650f);
+    public void AnimacionVertical(View view) {
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(imageViewLogo, "y", 530f);
         animatorY.setDuration(duracion);
-        ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(imageViewLogo,View.ALPHA,0.0f,1.0f);
+        ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(imageViewLogo, View.ALPHA, 0.0f, 1.0f);
         alphaAnimation.setDuration(duracion);
         AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(animatorY,alphaAnimation);
-        animatorSet.start();
-        }
-
-        public void handleAnimationHorizontal (View view){
-        ObjectAnimator animatorY = ObjectAnimator.ofFloat(imageViewLogo,"y",300);
-        animatorY.setDuration(duracion);
-        ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(imageViewLogo,View.ALPHA,0.0f,1.0f);
-        alphaAnimation.setDuration(duracion);
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(animatorY,alphaAnimation);
-        animatorSet.start();
-        }
-
-    public void handleAnimationVerticalLetra(View view){
-
-        ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(imageViewLetra,View.ALPHA,0.0f,1.0f);
-        alphaAnimation.setDuration(duracion);
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(alphaAnimation);
+        animatorSet.playTogether(animatorY, alphaAnimation);
         animatorSet.start();
     }
 
-    public void handleAnimationHorizontalLetra(View view){
-        ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(imageViewLogo,View.ALPHA,0.0f,1.0f);
+    public void animacionHorizontal(View view) {
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(imageViewLogo, "y", 300);
+        animatorY.setDuration(duracion);
+        ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(imageViewLogo, View.ALPHA, 0.0f, 1.0f);
         alphaAnimation.setDuration(duracion);
         AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(alphaAnimation);
+        animatorSet.playTogether(animatorY, alphaAnimation);
         animatorSet.start();
     }
 }
-
