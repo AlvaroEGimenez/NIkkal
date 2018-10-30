9c176c612e92d3395caca371c3293ad21b385b8a
Lady <lg.villamil.guerrero@gmail.com>
2018-10-30 00:29:19 -0300

    30.10favoritos
    ajustes view favoritos

--- app/src/main/java/com/digitalhouse/a0818moacn01_02/model/Favoritos.java
+++ app/src/main/java/com/digitalhouse/a0818moacn01_02/model/Favoritos.java
@@ -1,4 +1,46 @@
 package com.digitalhouse.a0818moacn01_02.model;
 
-public class Favoritos {
+import java.util.List;
+
+public class Favoritos  {
+
+    private String favoritoTexto;
+    private Integer favoritoImagen;
+
+    /*private String favoritoAlbum;
+    private String favoritoArtista;
+    private String favoritoPista;
+    private Integer favoritoImagen;
+    */
+    /*private List<Album> albumesFavoritos;
+    private List<Artista> artistasFavoritos;
+    private List<Pista> pistasFavoritas;*/
+
+    public Favoritos(String favoritoTexto, Integer favoritoImagen)/* List<Album> albumesFavoritos, List<Artista> artistasFavoritos, List<Pista> pistasFavoritas*/ {
+        this.favoritoTexto = favoritoTexto;
+        this.favoritoImagen = favoritoImagen;
+        /*this.albumesFavoritos = albumesFavoritos;
+        this.artistasFavoritos = artistasFavoritos;
+        this.pistasFavoritas = pistasFavoritas;*/
+    }
+
+    public String getFavoritoTexto() {
+        return favoritoTexto;
+    }
+
+    public Integer getFavoritoImagen() {
+        return favoritoImagen;
+    }
+
+    /*public List<Album> getAlbumesFavoritos() {
+        return albumesFavoritos;
+    }
+
+    public List<Artista> getArtistasFavoritos() {
+        return artistasFavoritos;
+    }
+
+    public List<Pista> getPistasFavoritas() {
+        return pistasFavoritas;
+    }*/
 }
--- app/src/main/java/com/digitalhouse/a0818moacn01_02/view/menuNavegacion/Favoritos/FavoritoFragment.java
+++ app/src/main/java/com/digitalhouse/a0818moacn01_02/view/menuNavegacion/Favoritos/FavoritoFragment.java
@@ -3,11 +3,17 @@ package com.digitalhouse.a0818moacn01_02.view.menuNavegacion.Favoritos;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.digitalhouse.a0818moacn01_02.R;
+import com.digitalhouse.a0818moacn01_02.model.Favoritos;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -21,11 +27,27 @@ public class FavoritoFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_favorito, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorito, container, false);
+        List<Favoritos> listaDeFavoritos = new ArrayList<>();
+        listaDeFavoritos.add(new Favoritos(getResources().getString(R.string.tv_almbum_favoritos),R.drawable.ic_album_24dp));
+        listaDeFavoritos.add(new Favoritos(getResources().getString(R.string.tv_artistas_favoritos),R.drawable.ic_artista));
+        listaDeFavoritos.add(new Favoritos(getResources().getString(R.string.tv_canciones_favoritas),R.drawable.ic_pista));
+        listaDeFavoritos.add(new Favoritos(getResources().getString(R.string.tv_mis_playlist),R.drawable.ic_listas_de_reproduccion));
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerFavoritos);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext(),LinearLayoutManager.VERTICAL,false));
+        recyclerView.setHasFixedSize(true);
+        AdaptadorFavoritos adaptadorFavoritos = new AdaptadorFavoritos(listaDeFavoritos);
+        recyclerView.setAdapter(adaptadorFavoritos);
+
+
+        return view;
+
     }
 
 }
