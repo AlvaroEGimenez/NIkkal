24fd8040f57277ad1805f385b677ae76c4640c45
Unknown <julian.misiak@gmail.com>
2018-11-04 11:42:31 -0300

    cambio mal relizado

--- app/src/main/java/com/digitalhouse/a0818moacn01_02/controller/GenreController.java
+++ app/src/main/java/com/digitalhouse/a0818moacn01_02/controller/GenreController.java
@@ -5,21 +5,60 @@ import android.content.Context;
 import com.digitalhouse.a0818moacn01_02.DAO.GenreDAO;
 import com.digitalhouse.a0818moacn01_02.Utils.ResultListener;
 import com.digitalhouse.a0818moacn01_02.Utils.Util;
+import com.digitalhouse.a0818moacn01_02.model.AlbumDeezer;
+import com.digitalhouse.a0818moacn01_02.model.ArtistDeezer;
 import com.digitalhouse.a0818moacn01_02.model.Genre;
 
 import java.util.List;
 
 public class GenreController {
+    private GenreDAO genreDAO = new GenreDAO();
 
     public void getGenre(final ResultListener<List<Genre>> listenerView, Context context) {
         if (Util.hayInternet(context)) {
-            GenreDAO genreDAO = new GenreDAO();
             genreDAO.getGenre(new ResultListener<List<Genre>>() {
                 @Override
                 public void finish(List<Genre> resultado) {
                     listenerView.finish(resultado);
                 }
             });
+
+        }
+    }
+
+    public void getArtistAlbum(final ResultListener<List<AlbumDeezer>> listenerView, Context context, Integer artistId) {
+        if (Util.hayInternet(context)) {
+            genreDAO.getArtistAlbum(new ResultListener<List<AlbumDeezer>>() {
+                @Override
+                public void finish(List<AlbumDeezer> resultado) {
+                    listenerView.finish(resultado);
+
+                }
+            }, artistId);
+
+        }
+    }
+
+    public void getArtist(final ResultListener<List<ArtistDeezer>> listenerView, Context context, Integer genreId) {
+        if (Util.hayInternet(context)) {
+            genreDAO.getArtist(new ResultListener<List<ArtistDeezer>>() {
+                @Override
+                public void finish(List<ArtistDeezer> resultado) {
+                    listenerView.finish(resultado);
+                }
+            }, genreId);
+
+        }
+    }
+
+    public void getPistas(final ResultListener<List<ArtistDeezer>> listenerView, Context context, Integer genreId) {
+        if (Util.hayInternet(context)) {
+            genreDAO.getArtist(new ResultListener<List<ArtistDeezer>>() {
+                @Override
+                public void finish(List<ArtistDeezer> resultado) {
+                    listenerView.finish(resultado);
+                }
+            }, genreId);
 
         }
     }
